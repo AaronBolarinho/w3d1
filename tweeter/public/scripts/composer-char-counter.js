86deb87a222ae3f8f197b2bb2e9a652992764e6d
AaronBolarinho <aaron.bolarinho@gmail.com>
2018-10-10 18:41:05 +0000

    finished with new tweet css and hover effect

--- tweeter/public/scripts/composer-char-counter.js
+++ tweeter/public/scripts/composer-char-counter.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-  $( "#tweeterText" ).on( "keydown", function() {
+  $( "#tweeterText" ).on( "input", function() {
     var count = $("#tweeterText").val().length
     $('#charCounter').html(140 - count);
-      if (count > 140) {
-        console.log(count)
-        $("#charCounter").css("color", "red");
-      } else {
-        $("#charCounter").css("color", "black");
-      };
+    if (count > 140) {
+      console.log(count)
+      $( "#charCounter" ).addClass( "overfillCounter" );
+    } else {
+      $( "#charCounter" ).removeClass( "overfillCounter" );
+    }
   });
 });
 
 
 
 // Add listener
+
