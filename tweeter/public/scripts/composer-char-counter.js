590807d719dfaeb7503100c7597d9172e12f6c0c
AaronBolarinho <aaron.bolarinho@gmail.com>
2018-10-10 03:26:01 +0000

    finished character counter, got the colours working

--- tweeter/public/scripts/composer-char-counter.js
+++ tweeter/public/scripts/composer-char-counter.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-
   $( "#tweeterText" ).on( "keydown", function() {
     var count = $("#tweeterText").val().length
-
     $('#charCounter').html(140 - count);
-      });
-
+      if (count > 140) {
+        console.log(count)
+        $("#charCounter").css("color", "red");
+      } else {
+        $("#charCounter").css("color", "black");
+      };
   });
+});
 
 
 
 // Add listener
  // '#charCounter'.addEventListener('keypress', callback);
+
+
+ // $("#charCounter").css("color", "red");
+ //      } else {
+ //        $("#charCounter").css("color", "black");
+ //      };
